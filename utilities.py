a0017c95dc97ca1770c20e4bef1f521cdaecaeee
Sam <doyon.sam1@gmail.com>
2026-05-28 21:59:19 -0600

    very wip

--- utilities.py
+++ utilities.py
@@ -32,8 +32,29 @@ class RogiDataset():
             for f in self.path.glob("*__horizontal_well.csv")
             }
         self.keys = list(self.well_files.keys())
+        self._index=0
 
     def __getitem__(self,key:str|int):
         if isinstance(key, int):
             key = self.keys[key]
-        return pd.read_csv(self.well_files[key]["Well"]),pd.read_csv(self.well_files[key]["TypeWell"])
+        well_data, typewell_data = pd.read_csv(self.well_files[key]["Well"]),pd.read_csv(self.well_files[key]["TypeWell"])
+        well_data, typewell_data = well_data.set_index("MD").sort_index(), typewell_data.set_index("TVT").sort_index()
+        pred_start_idx = well_data.index[(~well_data.TVT_input.isna()).sum()]
+        last_known_idx = well_data.index[~well_data.TVT_input.isna()][-1]
+        well_data["Known"] = well_data.index<=last_known_idx
+        return well_data, typewell_data
+
+    def __len__(self):
+        return len(self.keys)
+    
+    def __iter__(self):
+        self._index=0
+        return self
+
+    def __next__(self):
+        if self._index>= len(self):
+            raise StopIteration
+        val = self[self._index]
+        self._index+=1
+        return val
+        
